4c0051492c9909fbae3dd8ebae071dd46b944870
Christoph Hilty <christoph.hilty@gmail.com>
2019-01-01 16:33:33 +0100

    Added data with some wrangling to the app.

--- coding-exercises/flask_app/worldbankapp/routes.py
+++ coding-exercises/flask_app/worldbankapp/routes.py
@@ -1,5 +1,9 @@
 from worldbankapp import app
 from flask import render_template
+from wrangling_scripts.wrangling import get_data
+
+data = get_data()
+print(data)
 
 @app.route('/')
 @app.route('/index')
